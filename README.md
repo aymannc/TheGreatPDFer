e96144036837f891986c9fb7ad3af4ea3dbbca30
Ayman Nait Cherif <ayman.nait21@gmail.com>
2020-01-28 03:19:58 +0100

    version 1.0 non auth and limited pages

---
+++ README.md
@@ -0,0 +1,37 @@
+# The great PDFer (v 1.0)
+
+Download a book as a pdf format from **scholarvox**.
+
+## Prerequisites
+* You'll need python installed on a windows machine .
+* Latest version of google chrome (currently supporting chrome only).
+
+## Installing
+
+Clone the directory on your local machine  
+```
+git clone https://github.com/aymannc/TheGreatPDFer
+ ```
+After cloning the project,install the "requirements.txt" on your virtualenv
+
+```
+pip install -r requirements.txt
+```
+## Running the programme
+* Open the cmd on the containing folder and use 
+```
+(myenv) D:\EgyBest_1.0> python main.py
+```
+## Built With
+
+* [Selenium](https://selenium.dev/) - It's library used for browser automation
+* [fpdf](https://pyfpdf.readthedocs.io/en/latest/) - PyFPDF is a library for PDF document generation under Python
+* [PIL](https://pillow.readthedocs.io/en/stable/) -  PIL is the Python Imaging Library
+
+## Authors
+
+* **Nait Cherif Ayman**- [aymannc](https://github.com/aymannc)
+
+## License
+
+This project is licensed under the MIT License 
--- main.py
+++ main.py
@@ -8,29 +8,34 @@ from selenium import webdriver
 x = 1300
 y = 1200
 off = 160
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--log-level=3')
-chrome_options.add_argument('--disable-logging')
-chrome_options.add_argument("--headless")
-chrome_options.add_argument(F"--window-size={x},{y}")
-driver = webdriver.Chrome(options=chrome_options)
-
+try:
+    print("Drivers initialization")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument('--disable-logging')
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument(F"--window-size={x},{y}")
+    driver = webdriver.Chrome(executable_path="./Driver/chromedriver.exe",
+                              options=chrome_options)
+except:
+    exit("Driver Error")
+print("Done !")
 URL = 'https://international.scholarvox.com/reader/docid/88865376/page/'
 pdf = FPDF(unit="pt", format=(x - 2 * off + 20, y + 50))
 pdf.set_auto_page_break(0)
 try:
     for i in range(1, 10):
-        name = F"screenshot{i}.png"
-        print(name)
+        name = F"page{i}"
+        print("Starting " + name)
         driver.get(URL + str(i))
         time.sleep(1.5)
         png = driver.get_screenshot_as_png()
         im = Image.open(BytesIO(png))
         output_img = im.crop((off, 0, x - off - 40, y))
-        output_img.save(name)
+        output_img.save(name + ".png")
         pdf.add_page()
-        pdf.image(name)
+        pdf.image(name + ".png")
+        print(name.split(".")[0], "done !")
     pdf.output("yourfile.pdf", "F")
     driver.quit()
 except Exception as ignored:
